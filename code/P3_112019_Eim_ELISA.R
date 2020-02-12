1de5b80caaa35d6a6164f90927e11f761b9db6c7
LubomirBednar <lubomir.luke.bednar@gmail.com>
2020-02-12 13:19:32 +0100

    update ELISa processing script + adjust STDs to include 0s and avoid NAs in ED

--- code/P3_112019_Eim_ELISA.R
+++ code/P3_112019_Eim_ELISA.R
@@ -6,12 +6,31 @@ library(Rmisc)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+library(reshape2)
+library(drc)
 
 ##### add clean tables
-E1 <- "https://raw.githubusercontent.com/LubomirBednar/Manuscript_1/master/clean_data/P3_112019_Eim_feces_ELISA1_clean.csv"
-E1 <- read.csv(text = getURL(E1), sep = ";", header = FALSE)
+E1_std <- "https://raw.githubusercontent.com/LubomirBednar/Manuscript_1/master/clean_data/P3_112019_Eim_feces_ELISA1_std.csv"
+E1_std <- read.csv(text = getURL(E1_std))
 
-E1_labels <- "https://raw.githubusercontent.com/LubomirBednar/Manuscript_1/master/clean_data/P3_112019_Eim_feces_ELISA1_labels.csv"
-E1_labels <- read.csv(text = getURL(E1_labels), header = FALSE)
+E1_samples <- "https://raw.githubusercontent.com/LubomirBednar/Manuscript_1/master/clean_data/P3_112019_Eim_feces_ELISA1_samples.csv"
+E1_samples <- read.csv(text = getURL(E1_samples))
 
-# have to melt to long format and merge or reorganize the clean data to long format
+###### use drc to construct standard curve and pinpointprotein content
+
+model1<-drm(OD~Conc,
+            fct=LL.4(names=c("Slope", "Lower", "Upper", "ED50")),
+            data=E1_std)
+plot(model1)
+
+E1<-ED(model1,E1_samples$OD,type="absolute",display=F)
+row.names(E1) <- E1_samples$label
+
+points(y=E1_samples$OD,x=E1[,1],col="lightblue",pch=19,cex=2)
+text(y =E1_samples$OD, x = E1[,1], labels=E1_samples$label, data=E1, cex=0.9, font=2)
+
+E1 <- data.frame(E1)
+colnames(E1)[1] <- "IFNy"
+E1 <- dplyr::select(E1, IFNy)
+
+write.csv(E1, "./Manuscript_1/clean_data/P3_112019_Eim_feces_ELISA1_clean.csv")
